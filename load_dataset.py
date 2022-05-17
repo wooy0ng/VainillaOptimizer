826d84bfc28325691be0eec39effe24557070504
wooy0ng <yygg9800@naver.com>
2022-05-17 16:01:10 +0900

    second commit

--- load_dataset.py
+++ load_dataset.py
@@ -3,28 +3,42 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from sklearn.model_selection import train_test_split
 
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 
 class load_dataset(Dataset):
-    def __init__(self):
+    def __init__(self, mode='train'):
         super(load_dataset, self).__init__()
 
         df = pd.read_excel('./dataset.xlsx')
         labels_df = df['label']
         df.drop(['label'], axis=1, inplace=True)
         
-        # self.arrs = np.asarray(df).reshape(-1, 4, 3)
-        self.arrs = np.asarray(df)
-        self.labels = np.asarray(labels_df)
+    
+        arrs = np.asarray(df)
+        labels = np.asarray(labels_df)
 
+        train_X, test_X, train_y, test_y = train_test_split(
+            arrs, 
+            labels, 
+            test_size=0.2, 
+            random_state=42
+        )
+
+        if mode == 'train':
+            self._arrs = train_X
+            self._labels = train_y
+        else:
+            self._arrs = test_X
+            self._labels = test_y
     
     def __len__(self):
-        return len(self.arrs)
+        return len(self._arrs)
 
     def __getitem__(self, idx):
-        arr = self.arrs[idx]
-        label = np.asarray(self.labels[idx])
+        arr = self._arrs[idx]
+        label = np.asarray(self._labels[idx])
 
         return torch.tensor(arr), torch.tensor(label)
 
